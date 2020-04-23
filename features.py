7f89ae0fd547c110b3d83745f9fdcc49d4253811
Ben Hayes <ben@benhayes.net>
2020-04-23 23:08:09 +0100

    Implement feature extractors

--- features.py
+++ features.py
@@ -1,3 +1,12 @@
+"""
+Ben Hayes 2020
+
+ECS7013P Deep Learning for Audio & Music
+
+File: features.py
+Description: Feature extractors used on the front end of the network to create
+             the conditioned latent tuple.
+"""
 import crepe
 import librosa
 import numpy as np
@@ -10,20 +19,34 @@ from utils import SAMPLE_RATE, param_ranges
 
 
 class LoudnessEncoder(nn.Module):
+    """
+    A feature extractor for A-weighted loudness.
+    """
     def __init__(
             self,
-            data_mu,
-            data_std,
+            data_loud_mu,
+            data_loud_std,
             sr=SAMPLE_RATE,
             n_fft=1024,
             hop_length=256,
             out_size=250):
+        """
+        Construct an instance of LoudnessEncoder
 
+        Args:
+            data_loud_mu (torch.Tensor): The mean loudness of the dataset
+            data_loud_std (torch.Tensor): The standard deviation of the
+                loudness of the dataset
+            sr (int, optional): Sample rate. Defaults to SAMPLE_RATE.
+            n_fft (int, optional): FFT size. Defaults to 1024.
+            hop_length (int, optional): FFT hop length. Defaults to 256.
+            out_size (int, optional): Output length in frames. Defaults to 250.
+        """
         super().__init__()
         self.n_fft = n_fft
         self.hop_length = hop_length
-        self.data_mu = data_mu
-        self.data_std = data_std
+        self.data_loud_mu = data_loud_mu
+        self.data_loud_std = data_loud_std
         self.time_dim = out_size
 
         self.stft = Spectrogram(n_fft=n_fft, hop_length=hop_length)
@@ -36,17 +59,28 @@ class LoudnessEncoder(nn.Module):
         )
 
     def forward(self, x):
+        """
+        Extract A-weighted loudness from an audio tensor
+
+        Args:
+            x (torch.Tensor): A tensor of audio samples.
+
+        Returns:
+            torch.Tensor: A tensor of frame-wise loudness
+        """
         spec = self.stft(x)[:, :, :self.time_dim]
         db_spec = self.db(spec)
         weighted_db_spec =\
             self.weightings.repeat(x.shape[0], 1, db_spec.shape[-1]) + db_spec
         mag_spec = 10.0 ** (weighted_db_spec / 20.0)
-        return\
-            ((self.db(mag_spec.mean(dim=-2)) + self.data_mu) / self.data_std)\
-            .float().unsqueeze(-1)
+        return ((self.db(mag_spec.mean(dim=-2)) + self.data_loud_mu)
+                / self.data_loud_std).float().unsqueeze(-1)
 
 
 class F0Encoder(nn.Module):
+    """
+    A wrapper for fundamental frequency feature extractors.
+    """
     def __init__(
             self,
             sr=SAMPLE_RATE,
@@ -54,7 +88,18 @@ class F0Encoder(nn.Module):
             max_freq=param_ranges["f0"]["max"],
             hop_length=256,
             out_size=250):
+        """
+        Construct an instance of F0Encoder
 
+        Args:
+            sr (int, optional): Sample rate. Defaults to SAMPLE_RATE.
+            min_freq (float, optional): Minimum output frequency. Defaults to
+                param_ranges["f0"]["min"].
+            max_freq (float, optional): Maximum output frequency. Defaults to
+                param_ranges["f0"]["max"].
+            hop_length (int, optional): Hop length in samples. Defaults to 256.
+            out_size (int, optional): Output size in frames. Defaults to 250.
+        """
         super().__init__()
         self.sr = sr
         self.min_freq = min_freq
